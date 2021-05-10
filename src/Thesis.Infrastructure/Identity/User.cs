64d02ea98009486b3ef559b73fb72efed6e11a5a
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-10 11:41:22 +0200

    AppDbContext + abstract KeyApiAuthorizationDbContext

---
+++ src/Thesis.Infrastructure/Identity/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.Infrastructure.Identity
+{
+    public class AppUser : IdentityUser<int>
+    {
+    }
+    public class AppRole : IdentityRole<int>
+    {
+    }
+}
