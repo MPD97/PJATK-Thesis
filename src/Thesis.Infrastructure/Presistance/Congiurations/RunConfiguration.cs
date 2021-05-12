57b3644f9fb016f9605aa94ba41af8861309f254
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-12 15:22:14 +0200

    created fluentAPI RunConfiguration

---
+++ src/Thesis.Infrastructure/Presistance/Congiurations/RunConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Thesis.Domain.Entities;
+
+namespace Thesis.Infrastructure.Presistance.Congiurations
+{
+    public class RunConfiguration : IEntityTypeConfiguration<Run>
+    {
+        public void Configure(EntityTypeBuilder<Run> builder)
+        {
+            builder.HasKey(r => r.Id);
+
+            builder.HasMany(r => r.CompletedPoints)
+                .WithOne(cp => cp.Run)
+                .HasForeignKey(cp => cp.RunId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+}
