b9d87e91e8af3f400209fb787fbdbb3d8561de7d
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-12 15:19:28 +0200

    created fluentAPI CompletedPointsConfiguration

---
+++ src/Thesis.Infrastructure/Presistance/Congiurations/CompletedPointsConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Thesis.Domain.Entities;
+
+namespace Thesis.Infrastructure.Presistance.Congiurations
+{
+    public class CompletedPointsConfiguration : IEntityTypeConfiguration<CompletedPoints>
+    {
+        public void Configure(EntityTypeBuilder<CompletedPoints> builder)
+        {
+            builder.HasKey(cp => cp.Id);
+        }
+    }
+}
