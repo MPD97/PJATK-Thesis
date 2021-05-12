cfa9d217cb2a5c679fd8066013f4680c3b147318
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-12 16:33:03 +0200

    added generic repository with transactions

---
+++ src/Thesis.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Thesis.Domain.Entities.Abstract;
+
+namespace Thesis.Application.Common.Interfaces
+{
+    public interface IRepository<T> : IDisposable where T : BaseEntity
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> FindBy(Expression<Func<T, bool>> predicate);
+        IQueryable<T> FindActiveRecordsBy(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task SaveChangesAsync();
+        Task Delete(T entity);
+        Task UpdateAsync(T entity);
+        ITransaction BeginTransaction();
+    }
+}
---
+++ src/Thesis.Application/Common/Interfaces/ITransaction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Thesis.Application.Common.Interfaces
+{
+    public interface ITransaction : IDisposable
+    {
+        void Commit();
+        void Rollback();
+    }
+}
