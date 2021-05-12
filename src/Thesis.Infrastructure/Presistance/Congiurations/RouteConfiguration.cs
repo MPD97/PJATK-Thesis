4e704b42ad3a510303efa42e0eb60ec9ffae1397
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-12 10:47:59 +0200

    created fluentAPI UserConfiguration

---
+++ src/Thesis.Infrastructure/Presistance/Congiurations/RouteConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thesis.Domain.Entities;
+using Thesis.Infrastructure.Identity;
+
+namespace Thesis.Infrastructure.Presistance.Congiurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.HasMany(u => u.CreatedRoutes)
+                .WithOne()
+                .HasForeignKey(r => r.CreatedBy)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(u => u.ModifiedRoutes)
+                .WithOne()
+                .HasForeignKey(r => r.LastModifiedBy)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(u => u.Runs)
+                .WithOne()
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
