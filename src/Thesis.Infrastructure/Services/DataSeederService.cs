7f3ab25ae72f409420c66d5957a8aa97d976904a
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-17 08:18:05 +0200

    DataSeederService DI

--- src/Thesis.Infrastructure/Services/DataSeederService.cs
+++ src/Thesis.Infrastructure/Services/DataSeederService.cs
@@ -1,14 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Thesis.Application.Common.Interfaces;
+using Thesis.Infrastructure.Identity;
+using Thesis.Infrastructure.Presistance;
 
 namespace Thesis.Infrastructure.Services
 {
     public class DataSeederService : IDataSeederService
     {
+        private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public DataSeederService(AppDbContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         public Task CreateTestRoute()
         {
             throw new NotImplementedException();
