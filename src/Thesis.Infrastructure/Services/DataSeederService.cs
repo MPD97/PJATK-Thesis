0b8e6dc75ac56bd49ac5c6afeb96cba829426d1b
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-17 08:16:37 +0200

    added IDataSeederService and DataSeederService

---
+++ src/Thesis.Infrastructure/Services/DataSeederService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thesis.Application.Common.Interfaces;
+
+namespace Thesis.Infrastructure.Services
+{
+    public class DataSeederService : IDataSeederService
+    {
+        public Task CreateTestRoute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task CreateTestUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
