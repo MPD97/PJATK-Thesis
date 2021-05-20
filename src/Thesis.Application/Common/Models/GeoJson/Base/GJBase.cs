52b9783272cddc5f2121b3a8f29f5883bfe2d783
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-20 22:17:27 +0200

    GeoJsonLine

--- src/Thesis.Application/Common/Models/GeoJson/Base/GJBase.cs
+++ src/Thesis.Application/Common/Models/GeoJson/Base/GJBase.cs
@@ -1,40 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Thesis.Application.Common.Models.GeoJson.Base
+﻿namespace Thesis.Application.Common.Models.GeoJson.Base
 {
     public abstract class GJBase
     {
         public string Type { get; set; } = "geojson";
-        public virtual GJDataBase Data { get; set; } = new();
-    }
-
-    public class GeoJsonLine : GJBase
-    {
-        public string Type { get; set; } = "geojson";
-        public GeoJsonData Data { get; set; } = new();
-
-        public class GeoJsonData
-        {
-            public string Type { get; set; } = "Feature";
-            public GeoJsonProperties Properties { get; set; } = new();
-            public GeoJsonGeometry Geometry { get; set; } = new();
-
-
-            public class GeoJsonProperties
-            {
-
-            }
-
-            public class GeoJsonGeometry
-            {
-                public string Type { get; set; }
-
-                public decimal[,] GeoJsonCoordinates { get; set; }
-            }
-        }
+        public virtual GJDataBase Data { get; set; } 
     }
 }
---
+++ src/Thesis.Application/Common/Models/GeoJson/Line/GJLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thesis.Application.Common.Models.GeoJson.Base;
+
+namespace Thesis.Application.Common.Models.GeoJson.Line
+{
+    public class GJLine : GJBase
+    {
+        public new GJDataLine Data { get; set; } = new();
+    }
+}
