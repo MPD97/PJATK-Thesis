daf804c441065179b2f909521d8670d70a2e13af
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-05-20 21:41:47 +0200

    GeoJson base

---
+++ src/Thesis.Application/Common/Extensions/GeoJsonExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.Application.Common.Extensions
+{
+    public static class GeoJsonExtension
+    {
+        
+    }
+}
---
+++ src/Thesis.Application/Common/Models/GeoJsonRoute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thesis.Application.Common.Models
+{
+    public abstract class GeoJsonBase
+    {
+        public string Type { get; set; } = "geojson";
+        public GeoJsonData Data { get; set; } = new ();
+
+        public class GeoJsonData
+        {
+            public string Type { get; set; } = "Feature";
+            public GeoJsonProperties Properties { get; set; } = new ();
+            public GeoJsonGeometry Geometry { get; set; } = new ();
+
+
+            public class GeoJsonProperties
+            {
+
+            }
+
+            public class GeoJsonGeometry
+            {
+                public string Type { get; set; } = "LineString";
+
+                public decimal[,] GeoJsonCoordinates { get; set; }
+            }
+        }
+    }
+}
