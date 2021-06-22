5bc3833061751a8d87d53d4180466d47f2984e75
Mateusz Ambroziak <s16852@pjwstk.edu.pl>
2021-06-22 14:49:46 +0200

    idk

--- src/Thesis.WebUI/Client/DataServices/IRunServiceHttp.cs
+++ src/Thesis.WebUI/Client/DataServices/IRunServiceHttp.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 using Thesis.Application.Common.Routes.Commands.CreateRun;
 
 namespace Thesis.WebUI.Client.DataServices
--- src/Thesis.WebUI/Client/Pages/Map.cs
+++ src/Thesis.WebUI/Client/Pages/Map.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Thesis.Application.Common.Routes.Commands.CreateRun;
 using Thesis.Application.Common.Routes.Queries.GetRoutes;
@@ -24,6 +28,24 @@ namespace Thesis.WebUI.Client.Pages
 
         [Inject]
         private IRunServiceHttp _runService { get; set; }
+
+        [Inject]
+        private NavigationManager UriHelper { get; set; }
+
+        [CascadingParameter]
+        private Task<AuthenticationState> AuthenticationState { get; set; }
+        private ClaimsPrincipal User { get; set; }
+        private bool IsAuthenticated { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            User = (await AuthenticationState).User;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                IsAuthenticated = true;
+            }
+        }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
@@ -46,7 +68,13 @@ namespace Thesis.WebUI.Client.Pages
         [JSInvokable("CreateRun")]
         public async Task<RunDto> CreateRun(int routeId, decimal latitude, decimal longitude, int accuracy)
         {
-            return await _runService.CreateRun(routeId, latitude, longitude, accuracy);
+            if (!IsAuthenticated)
+            {
+                UriHelper.NavigateTo("login");
+            }
+            var result = await _runService.CreateRun(routeId, latitude, longitude, accuracy);
+
+            return result;
         }
     }
 }
